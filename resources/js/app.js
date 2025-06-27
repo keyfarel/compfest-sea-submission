6a4a4a7b5626ab7e5e195aeb9d8c1dc6effd7f18
keyfarel <carinsmddd@gmail.com>
2025-06-27 16:02:31 +0700

    feat(config): Register static images for Vite processing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,14 @@
 import './init/carousel';
 import './bootstrap';
+
+// Gambar untuk Halaman Home
+import '../../resources/images/home/healthy-salad.webp';
+import '../../resources/images/home/our-story.webp';
+
+// Gambar untuk Halaman Menu
+import '../../resources/images/menu/basic-plan.webp';
+import '../../resources/images/menu/premium-plan.webp';
+import '../../resources/images/menu/family-plan.webp';
+import '../../resources/images/menu/weight-loss-plan.webp';
+import '../../resources/images/menu/athlete-plan.webp';
+import '../../resources/images/menu/vegetarian-plan.webp';
